282bb0b7ef927e2178dfbbc2730c07ee808361b5
Kaeland Chatman <kaeland1@gmail.com>
2019-02-04 13:50:16 -0500

    Login works now.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import Navbar from './components/pages/Navbar'
-import 'semantic-ui-css/semantic.min.css'
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import Navbar from "./components/pages/Navbar";
+import LoginPage from "./components/pages/LoginPage";
+import "semantic-ui-css/semantic.min.css";
 
 class App extends Component {
   render() {
     return (
       <Router>
-        <Route path="/" exact component={Navbar} />
+        <div>
+          <Route path="/" exact component={Navbar} />
+          <Route path="/login" component={LoginPage} />
+        </div>
       </Router>
     );
   }
--- src/components/pages/LoginPage.js
+++ src/components/pages/LoginPage.js
@@ -1,21 +1,60 @@
-import React, { Component } from 'react'
-import { Segment, Button, Form } from 'semantic-ui-react'
-import { Link } from 'react-router-dom'
+import React, { Component } from "react";
+import { Segment, Form, Input, Button } from "semantic-ui-react";
+import { APP_URL } from '../../utils/routes'
 
 class LoginPage extends Component {
+  state = {
+    username: '',
+    password: ''
+  }
+
+  handleChange = (e, { value }) => {
+    this.setState({
+      [e.target.name]: value
+    })
+    console.log("Name: ", e.target.name, "Value: ", value);
+  };
+
+  login = (e) => {
+    e.preventDefault()
+    const { username, password } = this.state
+    const options = {
+      method: 'POST', 
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        user: {
+          username, 
+          password
+        }
+      })
+    }
+
+    fetch(`${APP_URL}/login`, options)
+      .then(res => res.json())
+      .then(console.log)
+  }
+
   render() {
     return (
-      <Form>
-        <Form.Field>
-          <label>Username</label>
-          <input placeholder="Username..."/>
-        </Form.Field>
-        <Form.Field>
-          <label>Password</label>
-          <input placeholder="Password" />
-        </Form.Field>
+      <Form onSubmit={this.login}>
+        <Form.Input
+          name="username"
+          label="Username"
+          placeholder="Username..."
+          onChange={this.handleChange}
+        />
+        <Form.Input
+          name="password"
+          label="Password"
+          placeholder="Password..."
+          onChange={this.handleChange}
+        />
         <Button type="submit">Submit</Button>
       </Form>
-    )
+    );
   }
 }
+
+export default LoginPage;
